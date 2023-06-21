338b12dc88b2817eb4f1d94bf48e6e8fbfc49923
Khalid Talakshi <khalid.talakshi@outlook.com>
2023-06-21 14:27:28 -0400

    Initial Commit

---
+++ Workout Log/Workout_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_LogApp.swift
+//  Workout Log
+//
+//  Created by Khalid Talakshi on 2023-06-21.
+//
+
+import SwiftUI
+
+@main
+struct Workout_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
